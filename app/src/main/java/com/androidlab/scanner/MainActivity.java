34e620a54fbc252ec38debf7aa2939a3f1da4fa7
Rohan H <rohanvh7@gmail.com>
2022-01-31 19:49:04 +0530

    Pushed from local to Remote

---
+++ app/src/main/java/com/androidlab/scanner/MainActivity.java
@@ -0,0 +1,72 @@
+package com.androidlab.scanner;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.budiyev.android.codescanner.CodeScanner;
+
+public class MainActivity extends AppCompatActivity{
+    private static final int RC_PERMISSION = 10;
+    private CodeScanner mCodeScanner;
+    private boolean mPermissionGranted;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mCodeScanner = new CodeScanner(this, findViewById(R.id.scanner_view));
+        mCodeScanner.setDecodeCallback(result -> runOnUiThread(() -> {
+            ScanResultDialog dialog = new ScanResultDialog(this, result);
+            dialog.setOnDismissListener(d -> mCodeScanner.startPreview());
+            dialog.show();
+        }));
+        mCodeScanner.setErrorCallback(error -> runOnUiThread(
+                () -> Toast.makeText(this, getString(R.string.scanner_error, error), Toast.LENGTH_LONG).show()));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                mPermissionGranted = false;
+                requestPermissions(new String[] {Manifest.permission.CAMERA}, RC_PERMISSION);
+            } else {
+                mPermissionGranted = true;
+            }
+        } else {
+            mPermissionGranted = true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == RC_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mPermissionGranted = true;
+                mCodeScanner.startPreview();
+            } else {
+                mPermissionGranted = false;
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mPermissionGranted) {
+            mCodeScanner.startPreview();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+        finish();
+    }
+}
